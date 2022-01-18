d9d0a35389f779bc39ebf2673e1353800a5dcbb0
Daniel Saavedra Fonnegra <daniel.saavedra.fon@gmail.com>
2022-01-18 11:17:23 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,9 +1,4 @@
 const api = require("./index")
 const request = require('supertest')
 
-describe('Inicio server', ()=>{
-    it('Health check express', async()=>{
-        const resp = await request(api).get('/healt-check')
-        expect(resp.statusCode).toEqual(200)
-    })
-})
+descri
